04eea3d55e0bff9736208d55fdfa48e0007dff86
Gokulan_S <gokulansaravanan191@gmail.com>
2024-11-29 23:15:57 +0530

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 async function Connect_MongoDB() {
   await mongoose.connect(
-    "mongodb+srv://krithik-0113:cFW3QeW6xk0WupR0@cluster0.y8z4nih.mongodb.net/userInfo?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://gokulan12:Welcome123@rbac.hg9t1.mongodb.net/?retryWrites=true&w=majority&appName=RBAC"
   );
   console.log("Database Connected Successfully");
 }
